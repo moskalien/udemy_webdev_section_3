4c2c337528119862d4ef7775c038569ba24b6e4d
Tony <anton.moskalenko@icloud.com>
2026-01-11 08:49:30 +0400

    section_activities done

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
-import "purecss/build/grids-responsive-min.css"
-import "purecss/build/grids-min.css"
+import "purecss/build/grids-min.css" //PURECSS GRIDS//
+import "purecss/build/grids-responsive-min.css" //PURECSS ADAPTIVE GRIDS//
 
 import "../sass/style.scss";
+
+
