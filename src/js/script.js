f287b6ef0b582c9fe5f249b286c1cbbbf22d8979
busy <test@Mac-mini.local>
2026-02-05 14:17:58 +0400

    images animations added

--- src/js/script.js
+++ src/js/script.js
@@ -122,6 +122,18 @@ try {
       errorsContainer: document
         .querySelector("#question")
         .parentElement.parentElement.querySelector(".checkbox-error-message"),
+    })
+    .onSuccess((event) => {
+      const form = event.currentTarget;
+      const formData = new FormData(form);
+
+      fetch("https://httpbin.org/post", {
+        method: "POST",
+        body: formData,
+      }).then(res => res.json()).then(data => {
+        console.log('Success', data);
+        form.reset();
+      })
     });
 } catch (e) {
 }
